02b3db6635841d53ffabae5b09e83225bd60bc19
Etienne Gobion <etienne@Mac-de-Etienne.local>
2018-12-07 07:40:21 -0800

    Firebase

---
+++ Pods/Target Support Files/Pods-Marmit/Pods-Marmit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Marmit : NSObject
+@end
+@implementation PodsDummy_Pods_Marmit
+@end
---
+++ Pods/Target Support Files/Pods-MarmitUITests/Pods-MarmitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarmitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MarmitUITests
+@end
