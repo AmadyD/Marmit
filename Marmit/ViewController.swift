2b1846f6d022363431ed1a4b070557b690324485
Etienne Gobion <etienne@mac-de-etienne.local>
2018-12-07 05:20:39 -0800

    Initial Commit

---
+++ Marmit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marmit
+//
+//  Created by Etienne Gobion on 07/12/2018.
+//  Copyright © 2018 Marmit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
